f582ccc1e9743ab8e0b814d99ce72fe9782c0bad
sriram <sriramalavalapatiit01@gmail.com>
2022-11-11 16:11:02 +0530

    day2 work

---
+++ data.js
@@ -0,0 +1,71 @@
+
+  let CHICKENBUCKETS=[
+    {
+        Item:"Peri Peri 10 Strips with Dynamite",
+        img:"https://orderserv-kfc-assets.yum.com/15895bb59f7b4bb588ee933f8cd5344a/images/items/xl/D-PR00002153.jpg?ver=21.88",
+        price:"639",
+        des:"10 Spicy Peri Peri chicken strips & our Dynamite Spicy Mayo Sauce Bottle" 
+    }
+    ,
+    {
+        Item:"Bucket for Two",
+        img:"https://orderserv-kfc-assets.yum.com/15895bb59f7b4bb588ee933f8cd5344a/images/items/xl/D-PR00000892.jpg?ver=21.88",
+        price:"599",
+        des:"Flat Rs. 115 off on combo of 2pc Hot & Crispy, 2 pc Smoky Red Chicken & Large Popcorn"   
+    }
+    ,
+    {
+        Item:"Peri Peri 10 Strips with Nashville",
+        img:"https://orderserv-kfc-assets.yum.com/15895bb59f7b4bb588ee933f8cd5344a/images/items/xl/D-PR00002154.jpg?ver=21.88",
+        price:"600",
+        des:"10 Spicy Peri Peri chicken strips & our Nashville Hot Sauce Bottle"   
+    }
+    ,
+    {
+        Item:"Peri Peri 10 Leg pc & 4 Dips",
+        img:"https://orderserv-kfc-assets.yum.com/15895bb59f7b4bb588ee933f8cd5344a/images/items/xl/D-PR00002155.jpg?ver=21.88",
+        price:"924",
+        des:"Leg before any wicket! Save 27% on 10 Spicy Peri Peri Leg Pieces & 4 Dips"   
+    }
+    ,
+    {
+        Item:"Peri Peri 5 Leg Pc Meal",
+        img:"https://orderserv-kfc-assets.yum.com/15895bb59f7b4bb588ee933f8cd5344a/images/items/xl/D-PR00002156.jpg?ver=21.88",
+        price:"628",
+        des:"5 Spicy Peri Peri Leg Pieces, 2 Dips, 1 Med Fries & a Pepsi PET" 
+    }
+    ,
+    {
+        Item:"Peri Peri 5 Leg Pc & 2 Dips",
+        img:"https://orderserv-kfc-assets.yum.com/15895bb59f7b4bb588ee933f8cd5344a/images/items/xl/D-PR00002157.jpg?ver=21.88",
+      price:"515",
+        des:"5 Spicy Peri Peri Leg Pieces, paired with 2 delicous dips"   
+    }
+    ,
+    {
+        Item:"Big 12",
+        img:"https://orderserv-kfc-assets.yum.com/15895bb59f7b4bb588ee933f8cd5344a/images/items/xl/D-PR00000854.jpg?ver=21.88",
+        price:"720",
+        des:"Save 28% on 6pc Hot & Crispy Chicken, 6 Hot Wings & 2 dips"  
+    }
+    ,
+    {
+        Item:"Ultimate Savings Bucket",
+        img:"https://orderserv-kfc-assets.yum.com/15895bb59f7b4bb588ee933f8cd5344a/images/items/xl/D-PR00000638.jpg?ver=21.88",
+    
+        price:"700",
+        des:"Save 33% on signature bucket of 4pc Hot & crispy, 6 Wings, 4 strips, 2 Dips & Pepsi PET"  
+    }
+    ,
+    {
+        Item:"5pc Smoky Red Chicken",
+        img:"https://orderserv-kfc-assets.yum.com/15895bb59f7b4bb588ee933f8cd5344a/images/items/xl/D-K445.jpg?ver=21.88",
+        price:"500",
+        des:"Flat 20% off on 5pc Smoky red grilled chicken"  
+    }
+]
+ 
+  localStorage.setItem("ChickenBuckets",JSON.stringify(CHICKENBUCKETS)) 
+         
+
+    
